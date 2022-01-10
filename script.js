65325205e1959649edf6d2526a33b78367b4c891
Gustavo Lemos <81194465+GustaJLemos@users.noreply.github.com>
2022-01-10 09:29:02 -0300

    alterando algumas coisas, e começando a pensar em novas funcionalidades

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const cont = document.querySelector('.container')
 const btn = document.querySelector('#btnDarkMode')
+// const label = document.querySelector('.button-purple')
 
 btn.addEventListener("click", () => {
     cont.classList.toggle("dark-mode")
+    // label.innerHTML = "Light Mode"
 })
 
